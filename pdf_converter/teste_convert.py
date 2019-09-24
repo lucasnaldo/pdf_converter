8bdd9ac330197896129f7c4b12f0e7fb0bca6a7a
Lucas Naldo <lucas.naldo@confitec.com.br>
2019-09-24 08:44:28 -0300

    correções

--- pdf_converter/teste_convert.py
+++ pdf_converter/teste_convert.py
@@ -1,7 +1,19 @@
 import pdftables_api
+import datetime
+import easygui
+import os
+
+arquivo = easygui.fileopenbox()
+inputFilepath = arquivo
+filename_w_ext = os.path.basename(inputFilepath)
+filename, file_extension = os.path.splitext(filename_w_ext)
+saida = (filename + '_Corrigido.xlsx')
 
 c = pdftables_api.Client('jnutwuhlq3r5')
-c.xlsx('teste2.pdf', 'output2.xlsx') 
+# dt = str(datetime.datetime.now().strftime("%Y%m%d"))
+# filename = '{}_convertido.xlsx'.format(dt)
+
+c.xlsx(arquivo, saida) 
 #replace c.xlsx with c.csv to convert to CSV
 #replace c.xlsx with c.xml to convert to XML
 #replace c.xlsx with c.html to convert to HTML
