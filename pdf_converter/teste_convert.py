8451204b2444eee76ac478643ddccb1ec7611d75
lucasnaldo <lucas_naldo@hotmail.com>
2019-09-24 00:36:28 -0300

    Add files via upload

---
+++ pdf_converter/teste_convert.py
@@ -0,0 +1,7 @@
+import pdftables_api
+
+c = pdftables_api.Client('jnutwuhlq3r5')
+c.xlsx('teste2.pdf', 'output2.xlsx') 
+#replace c.xlsx with c.csv to convert to CSV
+#replace c.xlsx with c.xml to convert to XML
+#replace c.xlsx with c.html to convert to HTML
